b4d212155647fe2703de23aa3fac9c2e18cc8d64
Joshua Philips <jmichaelphilips@gmail.com>
2017-07-30 17:51:25 -0500

    Created an example pr for sara..

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,10 @@ class Book < ApplicationRecord
   belongs_to :user
   has_many :requests
 
+  def sara
+    alksjflksajdf;aklsdjfkl;as
+  end 
+
   ACCEPTABLE_STATES = %w( listed requested accepted )
   validates_inclusion_of :state, in: ACCEPTABLE_STATES
   
